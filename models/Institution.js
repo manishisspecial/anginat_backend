e0d3fba1102f9a4a7870e2d49738a56d28c15910
mikeadesina <michaeladeshina015@gmail.com>
2024-11-17 15:06:10 +0100

    update institution model

--- models/Institution.js
+++ models/Institution.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const institutionSchema = new mongoose.Schema({
     institutionType: { type: String, enum: ['school', 'institute'], required: true },
+    institutionCode: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     name: {
         type: String,
         required: true,
