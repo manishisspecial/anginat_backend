fff86d0d23bf648560ba55628cff54206936c257
Taranjit <tarankhalsa3412@gmail.com>
2024-12-27 15:08:48 +0530

    Add get institute endpoint

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 const dotenv = require("dotenv");
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload');
-const errorMiddleware = require('./middlewares/error');
-const morgan = require('morgan');
-const authRoutes = require('./routes/authRoutes');
-const otpRoutes = require('./routes/otpRoutes');
-const leadRoutes = require('./routes/leadRoutes');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const errorMiddleware = require("./middlewares/error");
+const morgan = require("morgan");
+const authRoutes = require("./routes/authRoutes");
+const otpRoutes = require("./routes/otpRoutes");
+const leadRoutes = require("./routes/leadRoutes");
+const instituteRouter = require("./routes/instituteRoutes");
+
 dotenv.config();
 const app = express();
 const allowedOrigins = [
+<<<<<<< Updated upstream
     'http://localhost:5173',
     'https://learning.anginat.com',
     'https://www.springlearns.com',
     'https://testing.d2uojw7xfu916c.amplifyapp.com'
+=======
+  "http://localhost:5173",
+  "https://learning.anginat.com",
+  "https://www.springlearns.com",
+>>>>>>> Stashed changes
 ];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -36,22 +44,29 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+<<<<<<< Updated upstream
 app.use(morgan('dev'));
 app.use('/api/auth', authRoutes);
 app.use('/api/auth/otp', otpRoutes);
 app.use('/api/auth/lead', leadRoutes);
+=======
+app.use(morgan("dev"));
+app.use("/api/auth", authRoutes);
+app.use("/api/otp", otpRoutes);
+app.use("/api/lead", leadRoutes);
+app.use("/api/institute", instituteRouter);
+>>>>>>> Stashed changes
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Server is Running! 🚀');
+app.get("/", (req, res) => {
+  res.send("Server is Running! 🚀");
 });
-
 
 app.use(errorMiddleware);
 
 // 404 Not Found Handler
 app.use((req, res) => {
-    res.status(404).send('404: Page not found');
+  res.status(404).send("404: Page not found");
 });
 
 module.exports = app;
