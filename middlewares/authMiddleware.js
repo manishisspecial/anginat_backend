00b0a9cf83bf2e2edd8bf480a8dcf7db7bd0a229
Taranjit <tarankhalsa3412@gmail.com>
2024-12-31 14:55:21 +0530

    improve refresh token endpoint

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,38 +1,45 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-        return res.status(401).json({ message: 'Access token is missing or invalid' });
-    }
-    try {
-        const decoded = jwt.verify(token.split(' ')[1], process.env.ACCESS_TOKEN_SECRET);
-        req.user = {
-            id: decoded.id,
-            role: decoded.role,
-            institution: decoded.institution,
-        };
-        next();
-    } catch (err) {
-        return res.status(401).json({ message: 'Invalid or expired token' });
-    }
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access token is missing or invalid" });
+  }
+  try {
+    const decoded = jwt.verify(
+      token.split(" ")[1],
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    req.user = {
+      id: decoded.id,
+      role: decoded.role,
+      institution: decoded.institution,
+    };
+    console.log("accessToken is verified ", decoded);
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
 };
 const hasAccess = (requiredRoles) => (req, res, next) => {
-    if (!req.user) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    if (!Array.isArray(requiredRoles)) {
-        requiredRoles = [requiredRoles];
-    }
+  if (!Array.isArray(requiredRoles)) {
+    requiredRoles = [requiredRoles];
+  }
+  console.log("Required Roles:", requiredRoles);
+  console.log("User Role:", req.user.role);
+  if (!requiredRoles.includes(req.user.role)) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
 
-    if (!requiredRoles.includes(req.user.role)) {
-        return res.status(403).json({ message: 'Forbidden' });
-    }
-
-    next();
+  next();
 };
 
 module.exports = {
-    verifyToken,
-    hasAccess
+  verifyToken,
+  hasAccess,
 };
