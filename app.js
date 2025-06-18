ee811dba0f0ff2e89e58850ece04c29c8724b71c
Taranjit Singh <tarankhalsa3412@gmail.com>
2025-06-18 18:31:34 +0530

    add events contact us form

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3001",
+  "http://localhost:3000",
   "http://localhost:5172",
   "http://localhost:5173",
   "https://www.anginatlearning.com",
